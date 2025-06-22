44856d8144b60f26fa36af73a15cf140474ed37c
Rupanjan Pal <rupanjanpal54@gmail.com>
2025-06-23 01:07:56 +0530

    done with db setup

--- src/db.js
+++ src/db.js
@@ -7,3 +7,17 @@ export const getDB =async ()=>{
   const db =await fs.readFile(Db_Path,"utf-8");
   return JSON.parse(db);
 };
+export const saveDB =async(db)=>{
+  await fs.writeFile(Db_Path, JSON.stringify(db, null, 5));
+  return db;
+};
+export const insert =async (data)=>{
+  const db =await getDB();
+
+  if (!db.logs) db.logs= [];
+
+  db.logs.push(data);
+  
+  await saveDB(db);
+  return data;
+};
