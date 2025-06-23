99b6b5796a9df7dfe3c37b0b9ceb75f2267cbaa4
Rupanjan Pal <rupanjanpal54@gmail.com>
2025-06-24 02:35:39 +0530

    added command.js file

--- src/command.js
+++ src/command.js
@@ -1,5 +1,54 @@
+import { hideBin } from "yargs/helpers";
+import yargs from "yargs";
+import { fileURLToPath } from 'url';
+import fs from "fs/promises";
+
 const showLogs = (logs) => {
   logs.forEach((log) => {
     console.log(`[${new Date(log.id).toLocaleString()}] ${log.content}`);
   });
 };
+yargs(hideBin(process.argv))
+  .command(
+    "add something <message>",
+    "Add a new log entry",
+    (yargs) =>
+      yargs.positional("message", {
+        describe: "Log message",
+        type: "",
+      }),
+    async (argv) => {
+      const log = await newLog(argv.message);
+      console.log(" Log added:", log.content);
+      console.log(" Log added:", log.content);
+      console.log(" Log added:", log.content);
+      console.log(" Log added:", log.content);
+    }
+  )
+  .command(
+    "list","Show all logs",
+    () => {},
+    async () => {
+      const logs = await getAllLogs();
+      if (logs.length === 0) {
+        console.log("no logs '");
+        return;
+      }
+      showLogs(logs);
+    }
+  )
+  .command(
+    "test",
+    "Test the basic functionality",
+    () => {},
+    async () => {
+      const logs = await getAllLogs();
+      if (logs.length === 0) {
+        console.log("no logs '");
+        return;
+      }
+      showLogs(logs);
+    }
+    
+    
+  )
